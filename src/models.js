e5dff304abfdb55dd0e01736a418d711280a4508
dingwan <dingwan0331@gmail.com>
2022-12-10 18:29:46 +0900

    refactor: src/models.js에 모델클래스 초기화 로직 설정

--- src/models.js
+++ src/models.js
@@ -2,6 +2,9 @@ import { env } from "process";
 import Sequelize from "sequelize";
 import Config from "./config/sequelize.js";
 
+import * as cafeModels from "./apps/cafe/models.js";
+import * as reviewModels from "./apps/review/models.js";
+
 const environment = env.NODE_ENV;
 const databaseConfig = Config[environment];
 
@@ -11,6 +14,16 @@ const sequelize = new Sequelize(
   databaseConfig.password,
   databaseConfig
 );
+
+Object.values(cafeModels).forEach((model) => {
+  sequelize[model] = model;
+  model.init(sequelize);
+});
+
+Object.values(reviewModels).forEach((model) => {
+  sequelize[model] = model;
+  model.init(sequelize);
+});
 
 Object.values(sequelize.models).forEach((model) => {
   if (model.associate) {
