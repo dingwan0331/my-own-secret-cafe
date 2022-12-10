795043b07c0ad7ddcd2a30ca74e73da9fb030eb1
dingwan <dingwan0331@gmail.com>
2022-12-10 16:24:30 +0900

    feat: CafeImage Model 작성

--- src/apps/cafe/models.js
+++ src/apps/cafe/models.js
@@ -56,3 +56,32 @@ export class Cafe extends Sequelize.models {
     );
   }
 }
+
+export class CafeImage extends Sequelize.models {
+  static init(sequelize) {
+    return super.init(
+      {
+        id: {
+          type: Sequelize.BIGINT.UNSIGNED,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        url: {
+          type: Sequelize.STRING(200),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        modelName: "CafeImage",
+        tableName: "cafe_images",
+        paranoid: false,
+        underscored: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+}
