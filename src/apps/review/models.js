66a9c0d686bdc369c4d8b78f09c695ca886a4915
dingwan <dingwan0331@gmail.com>
2022-12-10 17:28:18 +0900

    feat: ReviewImage 모델 작성

--- src/apps/review/models.js
+++ src/apps/review/models.js
@@ -28,3 +28,32 @@ export class Review extends Sequelize.models {
     );
   }
 }
+
+export class ReviewImage extends Sequelize.models {
+  static init(sequelize) {
+    return super.init(
+      {
+        id: {
+          type: Sequelize.BIGINT.UNSIGNED,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        url: {
+          type: Sequelize.STRING(200),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        modelName: "ReviewImage",
+        tableName: "review_images",
+        paranoid: true,
+        underscored: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+}
