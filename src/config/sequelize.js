e361be7b97b420fa65972f35ecde60abf46dd9be
dingwan <dingwan0331@gmail.com>
2022-12-16 19:58:47 +0900

    refactor: sequelize config 클래스가 아닌 객체형태로 export

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -2,28 +2,28 @@ import { env } from "process";
 import dotenv from "dotenv";
 dotenv.config();
 
-export default class SequelizeConfig {
-  static development = {
+export default {
+  development: {
     username: env.DB_USERNAME,
     password: env.DB_PASSWORD,
     database: env.DB_NAME,
     host: env.DB_HOST,
     dialect: env.DB_DIALECT,
-  };
+  },
 
-  static test = {
+  test: {
     username: env.DB_USERNAME,
     password: env.DB_PASSWORD,
     database: env.DB_NAME,
     host: env.DB_HOST,
     dialect: env.DB_DIALECT,
-  };
+  },
 
-  static production = {
+  production: {
     username: env.DB_USERNAME,
     password: env.DB_PASSWORD,
     database: env.DB_NAME,
     host: env.DB_HOST,
     dialect: env.DB_DIALECT,
-  };
-}
+  },
+};
