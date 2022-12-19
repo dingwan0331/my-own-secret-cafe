fa7671ffa517c1eb4ec171d54981fa85774c5637
dingwan <dingwan0331@gmail.com>
2022-12-19 16:12:00 +0900

    refactor: customError super메서드 호출 추가

--- src/utils/error.js
+++ src/utils/error.js
@@ -1,31 +1,39 @@
 class BaseHttpError extends Error {
   constructor(message) {
-    this.status;
+    super();
     this.message = message;
     this.isCustom = true;
   }
 }
 
 export class BadRequestError extends BaseHttpError {
-  constructor() {
+  constructor(message) {
+    super(message);
     this.status = 400;
+    this.message = message;
   }
 }
 
 export class UnAuthrizedError extends BaseHttpError {
-  constructor() {
+  constructor(message) {
+    super(message);
     this.status = 401;
+    this.message = message;
   }
 }
 
 export class ForbiddenError extends BaseHttpError {
-  constructor() {
+  constructor(message) {
+    super(message);
     this.status = 403;
+    this.message = message;
   }
 }
 
 export class NotFoundError extends BaseHttpError {
   constructor() {
+    super();
     this.status = 404;
+    this.message = "Not Found Url";
   }
 }
